09f6dac9427781f16b2ca1d7fb3a9f22211252df
webpengun <webpengun@gmail.com>
2024-07-30 18:40:35 +0900

    テスト

--- 07.28.js
+++ 07.28.js
@@ -1,1 +1,34 @@
-あ
+class Student{
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    display(){
+
+        var a = 1;
+        var b= 2;
+
+        var sum = a + b ;
+        
+        return sum;
+
+    }
+
+    judge(sum) {
+
+        if(sum == 3){
+            console.log("3です");
+        }　else {
+            console.log("3じゃないです");
+        }
+    }
+
+
+}
+
+var test = new Student();
+
+var avg = test.display();
+
+test.judge(avg);
